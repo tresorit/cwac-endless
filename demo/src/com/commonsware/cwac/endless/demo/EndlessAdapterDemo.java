24a7833b7d4240ad1d59e92af30d64b87339b44d
Mark Murphy <mmurphy@commonsware.com>
2012-08-03 09:50:35 -0400

    refactored out DemoAdapter, now restarting animation after cfg change

--- demo/src/com/commonsware/cwac/endless/demo/EndlessAdapterDemo.java
+++ demo/src/com/commonsware/cwac/endless/demo/EndlessAdapterDemo.java
@@ -16,13 +16,6 @@ package com.commonsware.cwac.endless.demo;
 
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
-import android.widget.ArrayAdapter;
-import com.commonsware.cwac.endless.EndlessAdapter;
 import java.util.ArrayList;
 
 public class EndlessAdapterDemo extends ListActivity {
@@ -31,60 +24,24 @@ public class EndlessAdapterDemo extends ListActivity {
     super.onCreate(icicle);
     setContentView(R.layout.main);
     
-    ArrayList<Integer> items=new ArrayList<Integer>();
+    DemoAdapter adapter=(DemoAdapter)getLastNonConfigurationInstance();
     
-    for (int i=0;i<25;i++) { items.add(i); }
+    if (adapter==null) {
+      ArrayList<Integer> items=new ArrayList<Integer>();
+      
+      for (int i=0;i<25;i++) { items.add(i); }
+      
+      adapter=new DemoAdapter(this, items);
+    }
+    else {
+      adapter.startProgressAnimation();
+    }
     
-    setListAdapter(new DemoAdapter(items));
+    setListAdapter(adapter);
   }
   
-  class DemoAdapter extends EndlessAdapter {
-    private RotateAnimation rotate=null;
-    
-    DemoAdapter(ArrayList<Integer> list) {
-      super(new ArrayAdapter<Integer>(EndlessAdapterDemo.this,
-                                      R.layout.row,
-                                      android.R.id.text1,
-                                      list));
-      
-      rotate=new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF,
-                                  0.5f, Animation.RELATIVE_TO_SELF,
-                                  0.5f);
-      rotate.setDuration(600);
-      rotate.setRepeatMode(Animation.RESTART);
-      rotate.setRepeatCount(Animation.INFINITE);
-    }
-    
-    @Override
-    protected View getPendingView(ViewGroup parent) {
-      View row=getLayoutInflater().inflate(R.layout.row, null);
-      
-      View child=row.findViewById(android.R.id.text1);
-      
-      child.setVisibility(View.GONE);
-      
-      child=row.findViewById(R.id.throbber);
-      child.setVisibility(View.VISIBLE);
-      child.startAnimation(rotate);
-      
-      return(row);
-    }
-    
-    @Override
-    protected boolean cacheInBackground() {
-      SystemClock.sleep(10000);       // pretend to do work
-      
-      return(getWrappedAdapter().getCount()<75);
-    }
-    
-    @Override
-    protected void appendCachedData() {
-      if (getWrappedAdapter().getCount()<75) {
-        @SuppressWarnings("unchecked")
-        ArrayAdapter<Integer> a=(ArrayAdapter<Integer>)getWrappedAdapter();
-        
-        for (int i=0;i<25;i++) { a.add(a.getCount()); }
-      }
-    }
+  @Override
+  public Object getLastNonConfigurationInstance() {
+    return(getListAdapter());
   }
 }
--- demo/src/com/commonsware/cwac/endless/demo/EndlessAdapterFragment.java
+++ demo/src/com/commonsware/cwac/endless/demo/EndlessAdapterFragment.java
@@ -1,15 +1,8 @@
 package com.commonsware.cwac.endless.demo;
 
-import android.os.Bundle;
-import android.os.SystemClock;
 import android.app.ListFragment;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
-import android.widget.ArrayAdapter;
+import android.os.Bundle;
 import java.util.ArrayList;
-import com.commonsware.cwac.endless.EndlessAdapter;
 
 public class EndlessAdapterFragment extends ListFragment {
   DemoAdapter adapter=null;
@@ -25,61 +18,12 @@ public class EndlessAdapterFragment extends ListFragment {
       
       for (int i=0;i<25;i++) { items.add(i); }
       
-      adapter=new DemoAdapter(items);
+      adapter=new DemoAdapter(getActivity(), items);
+    }
+    else {
+      adapter.startProgressAnimation();
     }
     
     setListAdapter(adapter);
-  }
-
-  class DemoAdapter extends EndlessAdapter {
-    private RotateAnimation rotate=null;
-
-    DemoAdapter(ArrayList<Integer> list) {
-      super(new ArrayAdapter<Integer>(getActivity(), R.layout.row,
-                                      android.R.id.text1, list));
-
-      rotate=
-          new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF,
-                              0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-      rotate.setDuration(600);
-      rotate.setRepeatMode(Animation.RESTART);
-      rotate.setRepeatCount(Animation.INFINITE);
-    }
-
-    @Override
-    protected View getPendingView(ViewGroup parent) {
-      View row=
-          getActivity().getLayoutInflater().inflate(R.layout.row, null);
-
-      View child=row.findViewById(android.R.id.text1);
-
-      child.setVisibility(View.GONE);
-
-      child=row.findViewById(R.id.throbber);
-      child.setVisibility(View.VISIBLE);
-      child.startAnimation(rotate);
-
-      return(row);
-    }
-
-    @Override
-    protected boolean cacheInBackground() {
-      SystemClock.sleep(10000); // pretend to do work
-
-      return(getWrappedAdapter().getCount() < 75);
-    }
-
-    @Override
-    protected void appendCachedData() {
-      if (getWrappedAdapter().getCount() < 75) {
-        @SuppressWarnings("unchecked")
-        ArrayAdapter<Integer> a=
-            (ArrayAdapter<Integer>)getWrappedAdapter();
-
-        for (int i=0; i < 25; i++) {
-          a.add(a.getCount());
-        }
-      }
-    }
   }
 }
